8cfbbd2226140ee5757a768cc63679fbc0b304bb
jin <wlstjdpark@gmail.com>
2021-03-16 09:02:10 +0900

    webpack build test

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/app.js'
+
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js'
+  }
+}
